171b7e37a4653acba5454cabe82a3ac1175431dd
Anna Dabrowska <dabrowska@cosmocode.de>
2019-08-05 15:48:41 +0200

    PSR-2 cleanup

--- syntax/closesection.php
+++ syntax/closesection.php
@@ -1,21 +1,16 @@
 <?php
-if(!defined('DOKU_INC')) die();
-
-if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
-require_once(DOKU_PLUGIN.'syntax.php');
-
 /**
  * Copy of syntax_plugin_wrap_closesection
  * Used for special handling of headers
  */
-class syntax_plugin_ifauthex_closesection extends DokuWiki_Syntax_Plugin {
-
+class syntax_plugin_ifauthex_closesection extends DokuWiki_Syntax_Plugin
+{
     function getType() { return 'substition';}
     function getPType() { return 'block';}
     function getSort() { return 195; }
 
     /**
-     * Dummy handler, this syntax part has no syntax but is directly added to the instructions by the div syntax
+     * Dummy handler
      */
     function handle($match, $state, $pos, Doku_Handler $handler)
     {}
