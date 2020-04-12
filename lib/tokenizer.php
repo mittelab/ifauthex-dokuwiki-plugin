056319e0c9ac7d676c538964d84822389c261df5
Pietro Saccardi <lizardm4@gmail.com>
2020-04-12 02:39:58 +0200

    Implement multibyte support

--- lib/tokenizer.php
+++ lib/tokenizer.php
@@ -6,6 +6,58 @@ use \RuntimeException;
 
 require_once "exceptions.php";
 
+
+function mb_preg_adapt_regex($regex) {
+    if (strlen($regex) < 2) {
+        return array($regex, '');
+    }
+    // "/rgx/options" --> ["rgx", "options"]
+    $lastDelim = strrpos($regex, $regex[0]);
+    if ($lastDelim === false || $lastDelim < 1) {
+        return array($regex, '');
+    }
+    $options = substr($regex, $lastDelim + 1);
+    $regex = substr($regex, 1, $lastDelim - 1);
+    return array($regex, $options);
+}
+
+function mb_preg_match($text, $matchRegex, $position=0) {
+    mb_regex_encoding('UTF-8');
+    list($matchRegex, $matchRegexOptions) = mb_preg_adapt_regex($matchRegex);
+    $textPiece = mb_substr($text, $position);
+    if (mb_ereg_search_init($textPiece) === false) {
+        return null;
+    }
+    $result = mb_ereg_search_pos($matchRegex, $matchRegexOptions);
+    list($matchOfsBytes, $matchLenBytes) = $result;
+    // Is the match offset in byte coinciding with the specified position?
+    // Or: is the match exactly at $position?
+    if (mb_strlen(mb_strcut($textPiece, 0, $matchOfsBytes)) > 0) {
+        return null;
+    }
+    return mb_strcut($textPiece, $matchOfsBytes, $matchLenBytes);
+}
+
+
+function sb_preg_match($text, $matchRegex, $position=0) {
+    $matches = null;
+    $result = preg_match($matchRegex, $text, $matches, PREG_OFFSET_CAPTURE, $position);
+    if ($result === 0) {
+        return null;
+    } elseif ($result === false) {
+        throw new InvalidArgumentException('An error occurred in preg_match.');
+    } elseif (count($matches) == 0) {
+        throw new RuntimeException('No matches?');
+    }
+    list($matchTxt, $matchOfs) = $matches[0];
+    if ($matchOfs > $position) {
+        return null;
+    }
+    return $matchTxt;
+}
+
+
+
 class TokenDefinition {
     private $_representation = null;
     private $_name = null;
@@ -18,30 +70,32 @@ class TokenDefinition {
         }
         $this->_name = $name;
         if ($matchRegex === null) {
+            // preg_quote works also for multibyte
+            // https://stackoverflow.com/a/31733257/1749822
             $matchRegex = '/' . preg_quote($representation) . '/';
         }
         $this->_matchRegex = $matchRegex;
     }
 
-    public function representation() { return $this->_representation;     }
+    public function representation() { return $this->_representation; }
     public function name() { return $this->_name; }
 
-    public function tryMatch($text, $position) {
-        $matches = null;
-        $result = preg_match($this->_matchRegex, $text, $matches, PREG_OFFSET_CAPTURE, $position);
-        if ($result === 0) {
-            return null;
-        } elseif ($result === false) {
-            throw new InvalidArgumentException('An error occurred in preg_match.');
-        } elseif (count($matches) == 0) {
-            throw new RuntimeException('No matches?');
+    public static function supportsMultibyte() {
+        static $_loaded = null;
+        if ($_loaded === null) {
+            $_loaded = (extension_loaded('mbstring') === true);
         }
-        list($matchTxt, $matchOfs) = $matches[0];
-        if ($matchOfs > $position) {
-            return null;
-        }
-        return $matchTxt;
+        return $_loaded;
     }
+
+    public function tryMatch($text, $position) {
+        if (self::supportsMultibyte()) {
+            return mb_preg_match($text, $this->_matchRegex, $position);
+        } else {
+            return sb_preg_match($text, $this->_matchRegex, $position);
+        }
+    }
+
     public function __toString() {
         return '<' . $this->name() . '>';
     }
@@ -64,7 +118,13 @@ class TokenInstance {
     public function text() { return $this->_text; }
     public function position() { return $this->_position; }
     public function length() { return $this->_length; }
-    public function match() { return substr($this->_text, $this->position(), $this->length()); }
+    public function match() {
+        if (TokenDefinition::supportsMultibyte()) {
+            return mb_substr($this->_text, $this->position(), $this->length());
+        } else {
+            return substr($this->_text, $this->position(), $this->length());
+        }
+    }
 
     public function __toString() {
         return '<' . $this->definition()->name() . ':' . $this->match() . '>';
@@ -72,14 +132,25 @@ class TokenInstance {
 }
 
 function tokenize(string $text, array $tokDefs, array $stripTokDefs) {
+    if (TokenDefinition::supportsMultibyte()) {
+        $textLen = mb_strlen($text);
+    } else {
+        $textLen = strlen($text);
+    }
+
     $tokInsts = array();
     $foundTokInst = null;
-    for ($position = 0; $position < strlen($text); $position += $foundTokInst->length()) {
+    for ($position = 0; $position < $textLen; $position += $foundTokInst->length()) {
         $foundTokInst = null;
         foreach ($tokDefs as $tokDef) {
             $match = $tokDef->tryMatch($text, $position);
             if ($match !== null) {
-                $foundTokInst = new TokenInstance($tokDef, $text, $position, strlen($match));
+                if (TokenDefinition::supportsMultibyte()) {
+                    $matchLen = mb_strlen($match);
+                } else {
+                    $matchLen = strlen($match);
+                }
+                $foundTokInst = new TokenInstance($tokDef, $text, $position, $matchLen);
                 break;
             }
         }
